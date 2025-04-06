12612c60c80d371b841a07bb6ca2995e749e5f68
Subhajit Chakraborty <95707919+SubhChintak@users.noreply.github.com>
2025-04-06 18:07:03 +0530

    Update Hero.jsx

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/Picofme.png";
+import HeroImg from "../../images/me@j&k.jpg";
 import HeroBgAnimation from "../HeroBgAnimation";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
